1fe9f8570c766a0a6f85bea11e41595f04605938
IY0018 <alexey@ironyun.com>
2019-12-22 19:19:36 +0800

    VocabRow flip

--- lanternhsk/VocabRow.swift
+++ lanternhsk/VocabRow.swift
@@ -27,9 +27,28 @@ struct ImageButton: View {
     }
 }
 
-struct VocabRow: View {
+struct HeightPreferenceKey: PreferenceKey {
+    typealias Value = CGFloat
+
+    static var defaultValue: CGFloat = 0
+
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+struct VocabRowItemGeometry: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Rectangle()
+                .fill(Color.clear)
+                .preference(key: HeightPreferenceKey.self, value: geometry.size.height)
+        }
+    }
+}
+
+struct VocabRowSideOne: View {
     let card: VocabCard
-    
     @State var starred: Bool = false
     
     var body: some View {
@@ -46,8 +65,61 @@ struct VocabRow: View {
             VStack(alignment: .leading) {
                 Text(card.word).font(.title)
                 Text(card.pinyin).font(.subheadline)
-            }.frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding([.top, .bottom], 5.0)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(VocabRowItemGeometry())
             
+        }
+    }
+}
+
+struct VocabRowSideTwo: View {
+    let card: VocabCard
+    let maxHeight: CGFloat
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(card.translation)
+                .multilineTextAlignment(.leading)
+        }.frame(height: maxHeight)
+    }
+}
+
+struct VocabRow: View {
+    let card: VocabCard
+    @State private var flipped: Bool = false
+    @State var maxHeight : CGFloat
+    
+    init(card: VocabCard, height: CGFloat = 0.0) {
+        self.card = card
+        _maxHeight = State(initialValue: height)
+    }
+    
+    var body: some View {
+        ZStack {
+            VocabRowSideOne(card: card)
+                .rotation3DEffect(.degrees(self.flipped ? 180.0 : 0.0), axis: (x: 1.0, y: 0.0, z: 0.0))
+                .zIndex(self.flipped ? 0 : 1)
+                .opacity(self.flipped ? 0: 1)
+            
+            VocabRowSideTwo(card: card, maxHeight: maxHeight)
+                .rotation3DEffect(.degrees(self.flipped ? 0.0 : 180.0), axis: (x: -1.0, y: 0.0, z: 0.0))
+                .zIndex(self.flipped ? 1 : 0)
+                .opacity(self.flipped ? 1: 0)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture { self.handleFlipViewTap() }
+        .onPreferenceChange(HeightPreferenceKey.self) {
+            if $0 > self.maxHeight {
+                self.maxHeight = $0
+            }
+        }
+    }
+    
+    func handleFlipViewTap() -> Void {
+        withAnimation(.easeOut(duration:0.25)) {
+            self.flipped.toggle()
         }
     }
 }
