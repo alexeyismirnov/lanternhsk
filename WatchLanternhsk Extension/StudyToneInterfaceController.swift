ddec6e6bd54482f100e91797225ca0f00dd347dc
IY0018 <alexey@ironyun.com>
2020-02-28 16:10:58 +0800

    StudyTone for iWatch

--- WatchLanternhsk Extension/StudyToneInterfaceController.swift
+++ WatchLanternhsk Extension/StudyToneInterfaceController.swift
@@ -9,21 +9,39 @@
 import Foundation
 import WatchKit
 
+enum CheckmarkPhases {
+    case initialPoint
+    case downStroke
+    case upStroke
+    case error
+}
+
 class StudyToneInterfaceController: WKInterfaceController {
-    
     @IBOutlet weak var label1: WKInterfaceLabel!
     @IBOutlet weak var label2: WKInterfaceLabel!
     
     var previousPoint: CGPoint?
-    
+    var offsetX: CGFloat = 0.0
+    var offsetY: CGFloat = 0.0
+
     var deck: StudyDeck?
     let totalQuestions = 3
+    var totalCorrect = 0
+    var totalWrong = 0
+    
     var cards = [VocabCard]()
+    
     var index = 0
-
+    var toneIndex = 0
+    var tonePhase : CheckmarkPhases = .initialPoint
+    
+    var syllabi = [String]()
+    var tones = [Tone]()
+    var answers = [Bool]()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        setTitle("Draw tones")
+        setTitle("Cancel")
 
         deck = context as? StudyDeck
         
@@ -33,17 +51,77 @@ class StudyToneInterfaceController: WKInterfaceController {
             }
         }
         
+        getCard()
+    }
+    
+    func getCard(_ _index: Int = 0) {
+        index = _index
+        
+        if index == totalQuestions {
+            updateUI()
+            return
+        }
+        
+        syllabi = cards[index].pinyin.components(separatedBy: " ")
+        tones = cards[index].getTones()
+        
+        toneIndex = 0
+        answers = [Bool]()
+        
         updateUI()
     }
     
-    override func willActivate() {
-        super.willActivate()
-        
-    }
     
     func updateUI() {
-        label1.setText(cards[index].word)
-        label2.setText(cards[index].pinyin)
+        if (index == totalQuestions) {
+            label1.setText("Correct: \(totalCorrect)")
+            label2.setText("Wrong: \(totalWrong)")
+            return
+        }
+        
+        var word = NSAttributedString()
+        
+        for i in 0..<answers.count {
+            word = word + String(cards[index].word[i]).colored(with: answers[i] ? .green : .red)
+        }
+        
+        for i in answers.count..<syllabi.count {
+            word = word + String(cards[index].word[i])
+
+        }
+        
+        label1.setAttributedText(word)
+        
+        var str = [String]()
+        
+        for i in 0..<toneIndex {
+            str.append(syllabi[i])
+        }
+        
+        for i in toneIndex..<syllabi.count {
+            str.append(syllabi[i].folding(options: .diacriticInsensitive, locale: .current))
+        }
+        
+        label2.setText(str.joined(separator: " "))
+    }
+    
+    func toneAnswered(isCorrect: Bool) {
+        answers.append(isCorrect)
+        toneIndex += 1
+        updateUI()
+        
+        if toneIndex == syllabi.count {
+            if answers.contains(false) {
+                totalWrong += 1
+            } else {
+                totalCorrect += 1
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.getCard(self.index + 1)
+            }
+        }
+        
     }
     
     @IBAction func panGesture(_ sender: Any) {
@@ -51,30 +129,88 @@ class StudyToneInterfaceController: WKInterfaceController {
           return
         }
         
+        if index == totalQuestions || toneIndex == tones.count {
+            return
+        }
+        
         switch panGesture.state {
         case .began:
-          previousPoint = panGesture.locationInObject()
+            previousPoint = panGesture.locationInObject()
+            offsetX = 0.0
+            offsetY = 0.0
+            tonePhase = .initialPoint
 
         case .changed:
-          guard let previousPoint = previousPoint else {
-            return
-          }
-          let currentPanPoint = panGesture.locationInObject()
-          let deltaX = currentPanPoint.x - previousPoint.x
-          let deltaY = currentPanPoint.y - previousPoint.y
-          
-          print("\(deltaX) \(deltaY)")
+            guard let previousPoint = previousPoint else {
+                return
+            }
+            
+            let currentPoint = panGesture.locationInObject()
+            
+            let diffX = (currentPoint.x - previousPoint.x)
+            let diffY = (currentPoint.y - previousPoint.y)
+            
+            offsetX += diffX
+            offsetY += diffY
+            
+            self.previousPoint = currentPoint
+            
+            if tones[toneIndex] == .third {
+                switch tonePhase {
+                case .initialPoint:
+                    tonePhase =  (diffX >= 0.0 && diffY >= 0) ? .downStroke : .error
+                   
+                case .downStroke:
+                    if diffX >= 0.0 && diffY >= 0 {
+                        // downStroke
+                        
+                    } else if diffX >= 0.0 && diffY < 0.0 && offsetY > 40.0 {
+                        tonePhase = .upStroke
+                        offsetY = 0.0
+                        
+                    } else {
+                        tonePhase = .error
+                    }
+                    
+                case .upStroke:
+                    tonePhase = diffX >= 0.0 && diffY <= 0  ? .upStroke : .error
 
-          self.previousPoint = currentPanPoint
-
+                default:
+                    break
+                }
+            }
+            
+        case .ended:
+            switch tones[toneIndex] {
+            case .first:
+                toneAnswered(isCorrect: abs(offsetY) < 20.0 && offsetX > 40.0)
+               
+            case .second:
+                toneAnswered(isCorrect: offsetY < -40.0 && offsetX > 40.0)
+                
+            case .third:
+                toneAnswered(isCorrect: tonePhase == .upStroke && offsetY < -40.0)
+                
+            case .fourth:
+                toneAnswered(isCorrect: offsetY > 40.0 && offsetX > 40.0)
+               
+            default:
+                toneAnswered(isCorrect: false)
+            }
+            
         default:
-          previousPoint = nil
-          break
+            previousPoint = nil
+            break
         }
     }
     
     @IBAction func tapGesture(_ sender: Any) {
-        print("Tap gesture")
+        if index == totalQuestions || toneIndex == tones.count {
+            return
+        }
+        
+        toneAnswered(isCorrect: tones[toneIndex] == .none)
+
     }
 }
 
---
+++ lanternhsk/Extensions.swift
@@ -0,0 +1,87 @@
+//
+//  Extensions.swift
+//  lanternhsk
+//
+//  Created by Alexey Smirnov on 2/28/20.
+//  Copyright © 2020 Alexey Smirnov. All rights reserved.
+//
+
+import UIKit
+
+
+public extension String {
+    func colored(with color: UIColor) -> NSAttributedString {
+        return NSAttributedString(string: self).colored(with: color)
+    }
+    
+    var length: Int {
+        return count
+    }
+
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
+
+public extension NSAttributedString {
+    
+    var centered: NSAttributedString {
+        let centerStyle = NSMutableParagraphStyle()
+        centerStyle.alignment = .center
+        
+        return applying(attributes: [.paragraphStyle: centerStyle])
+    }
+    
+    func colored(with color: UIColor) -> NSAttributedString {
+        return applying(attributes: [.foregroundColor: color])
+    }
+    
+    func font(font: UIFont) -> NSAttributedString {
+        return applying(attributes: [.font: font])
+    }
+    
+    func systemFont(ofSize: CGFloat) -> NSAttributedString {
+        return applying(attributes: [.font: UIFont.systemFont(ofSize: CGFloat(ofSize))])
+    }
+    
+    func boldFont(ofSize: CGFloat) -> NSAttributedString {
+        return applying(attributes: [.font: UIFont.boldSystemFont(ofSize: CGFloat(ofSize))])
+    }
+    
+    fileprivate func applying(attributes: [NSAttributedString.Key: Any]) -> NSAttributedString {
+        let copy = NSMutableAttributedString(attributedString: self)
+        let range = (string as NSString).range(of: string)
+        copy.addAttributes(attributes, range: range)
+        
+        return copy
+    }
+    
+    static func += (lhs: inout NSAttributedString, rhs: NSAttributedString) {
+        let string = NSMutableAttributedString(attributedString: lhs)
+        string.append(rhs)
+        lhs = string
+    }
+    
+    static func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+        let string = NSMutableAttributedString(attributedString: lhs)
+        string.append(rhs)
+        return NSAttributedString(attributedString: string)
+    }
+    
+    static func += (lhs: inout NSAttributedString, rhs: String) {
+        lhs += NSAttributedString(string: rhs)
+    }
+    
+    static func + (lhs: NSAttributedString, rhs: String) -> NSAttributedString {
+        return lhs + NSAttributedString(string: rhs)
+    }
+    
+}
